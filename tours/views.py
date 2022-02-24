16c68b66d2c12a9c5fb9a7e1947ef9414522a01d
lte-oniip <lte@oniip.ru>
2022-02-24 15:21:37 +0600

    added data.py

--- tours/views.py
+++ tours/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponseNotFound, HttpResponseServerError
 
 
 def main_view(request):
-    return render(request, 'index.html')
+    return render(request, 'tours/index.html')
 
 
 def departure_view(request, departure):
-    return render(request, 'departure.html')
+    return render(request, 'tours/departure.html')
 
 
 def tour_view(request, id):
-    return render(request, 'tour.html')
+    return render(request, 'tours/tour.html')
 
 
 def custom_handler404(request, exception):
